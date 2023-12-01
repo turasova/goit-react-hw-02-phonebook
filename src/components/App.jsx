b8935009ee8b1ad35a552ec602fa98e4447c54f4
turasova <134943646+turasova@users.noreply.github.com>
2023-12-01 17:16:09 +0200

    phonebook

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,67 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from "react"
+import { Form } from "./Form/Form";
+import { ContactList } from "./ContactList/ContactList";
+import { nanoid } from "nanoid";
+import { Filter } from "./Filter/Filter";
+import css from './App.module.css';
+
+export class App extends Component {
+  state = {
+    contacts: [
+    {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
+    {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
+    {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
+    {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
+  ],
+  filter: ''
+  }
+
+  onCreateContact = (contacts) => {
+    const newContact = {
+      ...contacts,
+      id: nanoid(),
+    }
+    const isDuplicated = this.state.contacts.find((contact) => 
+      contact.name === contacts.name)
+    if (isDuplicated) return
+    this.setState((prev) => ({
+      contacts: [...prev.contacts, newContact ]
+    }))
+}
+
+  deleteContact = (id) => {
+    this.setState((prev) => ({
+      contacts: prev.contacts.filter((contact) => contact.id !==id)
+  }))
+  }
+  
+  changeFilter = (e) => {
+    const { value } = e.currentTarget;
+    this.setState({filter: value})
+  }
+  filterByName = () => {
+    const { contacts, filter } = this.state;
+    const lowerFilter = filter.toLowerCase();
+    return contacts.filter(({ name }) => 
+    (name.toLowerCase().includes(lowerFilter)))
+  }
+  
+  render() {
+    const visibleContacts = this.filterByName();
+    return (
+      <div>
+        <h1 className={css.title}>Phonebook</h1>
+        <Form onCreateContact = {this.onCreateContact} />
+        <h2 className={css.title}>Contacts</h2>
+        <Filter filter={this.filter}
+                changeFilter={this.changeFilter}
+        />
+        <ContactList
+          contacts={visibleContacts}
+          deleteContact={this.deleteContact}
+        />
+    
+      </div>
+    );
+  }
+}
